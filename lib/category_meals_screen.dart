bc2e110d2b2b0c068b60f05a55a30a6b57a6d0c9
Kartikaiya Mayatwal <kartike.mayatwal@artelir.com>
2024-03-10 12:37:00 +0530

    Using Rotue for Navigation

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
-  final String categoryId;
-  final String categoryTitle;
+  static const routeName = '/category-meals';
+  // final String categoryId;
+  // final String categoryTitle;
 
-  CategoryMealsScreen(this.categoryId, this.categoryTitle);
+  // CategoryMealsScreen(this.categoryId, this.categoryTitle);
 
   @override
   Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+
+    final String categoryId = routeArgs['id'];
+    final String categoryTitle = routeArgs['title'];
+
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import './category_screen.dart';
+import './category_meals_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -28,7 +29,12 @@ class MyApp extends StatelessWidget {
               ),
             ),
       ),
-      home: CategoriesScreen(),
+      // home: CategoriesScreen(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => CategoriesScreen(),
+        CategoryMealsScreen.routeName: (context) => CategoryMealsScreen(),
+      },
     );
   }
 }
